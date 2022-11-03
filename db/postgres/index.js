4e047d5653e1ae8796dc76d1f6cdca3e592a582b
Kee Wook Lee <keewook2@gmail.com>
2022-11-03 15:22:56 -0700

    first code of creating table schema postgres

--- db/postgres/index.js
+++ db/postgres/index.js
@@ -16,19 +16,48 @@ pg.connect((err) => {
   }
 });
 
-pg.query(`CREATE TABLE IF NOT EXISTS reviews (
-  id SERIAL PRIMARY KEY,
-  product_id int,
-  rating int,
-  summary VARCHAR,
-  recommend boolean,
-  response VARCHAR,
-  body VARCHAR,
-  date timestamp,
-  reviewer_name VARCHAR,
-  helpfulness int
-);`, (err, res) => {
-  console.log(err ? err.stack : res.rows);
-});
+const queries = {
+  createSchema: () => {
+    pg.query(`CREATE TABLE IF NOT EXISTS reviews (
+      id SERIAL PRIMARY KEY,
+      product_id int,
+      rating int,
+      summary VARCHAR,
+      recommend boolean,
+      response VARCHAR,
+      body VARCHAR,
+      date timestamp,
+      reviewer_name VARCHAR,
+      helpfulness int
+    );`, (err, res) => {
+      console.log(err ? err.stack : res.rows);
+    });
 
-module.exports = pg;
+    pg.query(`CREATE TABLE IF NOT EXISTS photos (
+      id SERIAL PRIMARY KEY,
+      url VARCHAR,
+      review_id int
+    );`, (err, res) => {
+      console.log(err ? err.stack : res.rows);
+    });
+
+    pg.query(`CREATE TABLE IF NOT EXISTS characteristics (
+      id SERIAL PRIMARY KEY,
+      product_id int,
+      name VARCHAR
+    );`, (err, res) => {
+      console.log(err ? err.stack : res.rows);
+    });
+
+    pg.query(`CREATE TABLE IF NOT EXISTS characteristics_reviews (
+      id SERIAL PRIMARY KEY,
+      review_id int,
+      characteristics_id int,
+      value int
+    );`, (err, res) => {
+      console.log(err ? err.stack : res.rows);
+    });
+  },
+};
+
+module.exports = queries;
