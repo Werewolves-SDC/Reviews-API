701996e0bafc0587530e5fd5c5bf71859fff8ce2
Kee Wook Lee <keewook2@gmail.com>
2022-11-04 15:41:27 -0700

    setting up API route and function for controllers

---
+++ server/controllers.js
@@ -0,0 +1,7 @@
+module.exports = {
+  getReviews: (req, res) => {},
+  getReviewMeta: (req, res) => {},
+  addReview: (req, res) => {},
+  putHelpful: (req, res) => {},
+  putReport: (req, res) => {},
+};
