60327fd29686c2e76ca7e07ac14ddfac9039bb17
Kee Wook Lee <keewook2@gmail.com>
2022-11-10 11:33:17 +0900

    env variable for deploy

--- db/postgres/index.js
+++ db/postgres/index.js
@@ -1,13 +1,23 @@
 const { Pool } = require('pg');
 require('dotenv').config();
 
+// Uncomment this and comment pool below in deployed one.
 const pool = new Pool({
-  user: 'keewooklee',
-  password: '',
-  port: 5432,
-  host: 'localhost',
-  database: 'reviewsdc',
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
 });
+
+// pool for local dev environment.
+// const pool = new Pool({
+//   user: 'keewooklee',
+//   password: '',
+//   port: 5432,
+//   host: 'localhost',
+//   database: 'reviewsdc',
+// });
 
 pool.on('connect', (client) => {
   console.log(`connected to DB(postgres) on port ${client.port}`);
